a5a421a234eb41539eba9de51ea81394e8c32970
caretaker <gryb1807@gmail.com>
2022-11-26 05:33:31 +0100

    swipe

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+window.onload = () => {
+  const touchData = {
+    touchendX: 0,
+    touchendY: 0,
+    touchstartX: 0,
+    touchstartY: 0,
+  };
+  const forecast = document.getElementsByClassName("forecast")[0];
+  const clOpened = "forecast--opened";
+
+  if (forecast) {
+    forecast.addEventListener(
+      "touchstart",
+      ({ changedTouches }) => {
+        touchData["touchstartX"] = changedTouches[0].screenX;
+        touchData["touchstartY"] = changedTouches[0].screenY;
+      },
+      false
+    );
+
+    forecast.addEventListener(
+      "touchend",
+      ({ changedTouches }) => {
+        const { touchendY, touchstartY } = touchData;
+
+        touchData["touchendX"] = changedTouches[0].screenX;
+        touchData["touchendY"] = changedTouches[0].screenY;
+
+        if (touchendY < touchstartY) {
+          forecast.classList.add(clOpened);
+          console.log("Swiped up");
+          console.log(forecast);
+        }
+
+        if (touchendY > touchstartY) {
+          forecast.classList.remove(clOpened);
+          console.log("Swiped down");
+        }
+      },
+      false
+    );
+  }
+};
